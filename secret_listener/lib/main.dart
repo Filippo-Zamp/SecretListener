4c7a864672b807d3daca0573517f80d68ffa75f5
Filippo Zampiron <filippo.zam@gmail.com>
2025-06-06 16:38:32 +0200

    last commit

--- secret_listener/lib/main.dart
+++ secret_listener/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:audioplayers/audioplayers.dart';
+
 
 void main() {
   runApp(const SecretListenerApp());
@@ -16,7 +19,7 @@ class SecretListenerApp extends StatelessWidget {
           seedColor: Colors.green,
           brightness: Brightness.dark
           )),
-      home: SecretListenerHomepage(), );
+      home: const SecretListenerHomepage(), );
   }
 }
 
@@ -28,6 +31,10 @@ class SecretListenerHomepage extends StatefulWidget {
 }
 
 class _SecretListenerHomepageState extends State<SecretListenerHomepage> {
+  FilePickerResult? result;
+  AudioPlayer audioPlayer = AudioPlayer();
+  double playbackRate = 1.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,18 +46,49 @@ class _SecretListenerHomepageState extends State<SecretListenerHomepage> {
         child: Column(
           children: [
             Image.network('https://static.vecteezy.com/system/resources/previews/023/986/631/original/whatsapp-logo-whatsapp-logo-transparent-whatsapp-icon-transparent-free-free-png.png',
-            height: 300,
+              height: 300,
             ),
-      const Text('Scegli un Vocale')
+            result == null ? const Text('Scegli un Vocale') : Text(result!.files.single.name),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  onPressed: playAudio(),
+                  icon: const Icon(Icons.play_arrow),
+                  iconSize: 100,
+                ),
+                ElevatedButton(onPressed: togglePlaybackRate, child: Text('x$playbackRate')),
+              ],
+            )
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: pickFile(),
+        onPressed: pickFile,
         child: const Icon(Icons.audio_file)
         ),
     );
   }
   
-  pickFile() {}
+  void pickFile() async {
+    result = await FilePicker.platform.pickFiles();
+    setState(() {
+      
+    });
+  }
+  
+  playAudio() {
+    if (result != null){
+      audioPlayer.play(DeviceFileSource(result!.files.single.path!));
+    }
+
+  }
+
+  void togglePlaybackRate() {
+    playbackRate = playbackRate == 1.0 ? 2.0 : 1.0;
+    audioPlayer.setPlaybackRate(playbackRate);
+    setState(() {
+      // Update the UI to reflect the new playback rate
+    });
+  }
 }
